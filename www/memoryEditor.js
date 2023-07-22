766bc8308a81088ef5771f05643025ae0e682a6c
Wang <gavin19950511@gmail.com>
2023-07-22 10:14:56 +0000

    dev(chatbot): Refactor js packae structure

---
+++ www/memoryEditor.js
@@ -0,0 +1,8 @@
+// import "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js"
+// import "https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ext-beautify.js"
+
+// var editor = ace.edit("editor2");
+// editor.setTheme("ace/theme/monokai");
+// editor.getSession().setMode("ace/mode/json");
+// editor.setReadOnly(true);
+// console.log("It's done.");
