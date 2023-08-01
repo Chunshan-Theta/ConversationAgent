5a924b78700afd299010c88bd0908d466a595d79
YXR9 <yangxuru870918@gmail.com>
2023-08-01 19:00:56 +0800

    feat(web): json editor

--- frontend/src/components/jsonEditor.tsx
+++ frontend/src/components/jsonEditor.tsx
@@ -1,57 +1,113 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-json";
 import "ace-builds/src-noconflict/theme-monokai";
 import {botSample} from "./sampleBot";
-function onChange(newValue: string) {
-  console.log("change", newValue);
-}
+
+
+
 type Editor = {
   editorId: string
 }
+
+
 function BotEditor(prop: Editor) {
+  console.log();
+  const [msgOfSystem, setMsgOfSystem] = useState("");
+  const [valueOfBot, setValueOfBot] = useState("{}");
+
   let divId: string = prop.editorId || "editor";
+  const initBot = () => {
+    setValueOfBot(JSON.stringify(botSample, null, 2));
+  };
+  const changeFunc = (value: string) => setValueOfBot(value);
+  const beautify = () => {
+    try {
+      
+      const obj = JSON.parse(valueOfBot);
+      setValueOfBot(JSON.stringify(obj, null, 2));
+      setMsgOfSystem("");
+      
+    } catch ({name, message}) {
+      setMsgOfSystem(name+": "+message);
+    }
+  
+  };
+
+  
   return (
     <div>
     <AceEditor
         mode="json"
         theme="monokai"
         name={divId}
-        onChange={onChange}
+        onChange={changeFunc}
         editorProps={{ $blockScrolling: true }}
         setOptions={{
-          useWorker: false
+          useWorker: false,
+          enableAutoIndent: true,
+          enableLiveAutocompletion: true
         }}
-        value={JSON.stringify(botSample, null, 2)}
+        value={valueOfBot}
         
       />
+    <button onClick={initBot}>Init</button>
+
+    <button onClick={beautify}>Beautify</button>
+    <div >{msgOfSystem}</div>
+    </div>
+  );
+  
+}
+function MemoryEditor(prop: Editor) {
+  console.log();
+  const [msgOfSystem, setMsgOfSystem] = useState("");
+  const [valueOfMemory, setValueOfMemory] = useState("{}");
+
+  let divId: string = prop.editorId || "editor";
+  const initMemory = () => setValueOfMemory("{}");
+
+  const changeFunc = (value: string) => setValueOfMemory(value);
+  const beautify = () => {
+    try {
+      
+      const obj = JSON.parse(valueOfMemory);
+      setValueOfMemory(JSON.stringify(obj, null, 2));
+      setMsgOfSystem("");
+      
+    } catch ({name, message}) {
+      setMsgOfSystem(name+": "+message);
+    }
+  
+  };
+
+  
+  return (
+    <div>
+    <AceEditor
+        mode="json"
+        theme="monokai"
+        name={divId}
+        onChange={changeFunc}
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{
+          useWorker: false,
+          enableAutoIndent: true,
+          enableLiveAutocompletion: true
+        }}
+        value={valueOfMemory}
+        
+      />
+    <button onClick={initMemory}>Init</button>
+
+    <button onClick={beautify}>Beautify</button>
+    <div >{msgOfSystem}</div>
     </div>
   );
   
 }
 
-function MemoryEditor(prop: Editor) {
-  let divId: string = prop.editorId || "editor";
-  return (
-    <div>
-    <AceEditor
-        mode="json"
-        theme="monokai"
-        name={divId}
-        onChange={onChange}
-        editorProps={{ $blockScrolling: true }}
-        setOptions={{
-          useWorker: false
-          
-        }}
-        value='{}'
-        
-      />
-    </div>
-  );
-  
-}
 
 export {BotEditor, MemoryEditor};
 
